caf4402ece1e94081cc5fa2f04c3f9bfb747b59f
Ayesha Momina <mcsf19m022@pucit.edu.pk>
2021-06-06 08:06:37 +0500

    DBHelper class implementend

---
+++ app/src/main/java/com/example/notes/DBHelper.java
@@ -0,0 +1,24 @@
+package com.example.notes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper (Context context)
+    {
+        super(context,"myDB",null,3);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "Create table notes (_id Integer Primary Key AutoIncrement, title text, content text)";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
